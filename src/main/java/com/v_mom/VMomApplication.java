367ed4459a168d0da4f46d661db5da392f0f8d41
Muhammad Aldawahreh <muhammadaldawahreh@gmail.com>
2025-04-07 20:24:47 +0300

    init

---
+++ src/main/java/com/v_mom/VMomApplication.java
@@ -0,0 +1,13 @@
+package com.v_mom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VMomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VMomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/v_mom/VMomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.v_mom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VMomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
